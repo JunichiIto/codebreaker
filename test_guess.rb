e5a167ee1555d4802319b8deb29796252ca07a2d
Yuji Shimoda <yuji.shimoda@gmail.com>
2013-09-25 16:55:27 +0900

    test_guess.rb update.

--- test_guess.rb
+++ test_guess.rb
@@ -2,89 +2,68 @@ require 'test/unit'
 require './guess.rb'
 
 class TestCase_CodeBreakerGuess < Test::Unit::TestCase
-  def test_confirm
+  def test_1234_confirm
     guess = CodeBreaker::Guess.new("1234")
+
     # Scenarios: no matches
     assert_equal("",      guess.confirm("5555"))
-  end
 
-  def test1_confirm
-    guess = CodeBreaker::Guess.new("1234")
     # Scenarios: 1 number correct
     assert_equal("+",     guess.confirm("1555"))
     assert_equal("-",     guess.confirm("2555"))
-  end
 
-  def test2_confirm
-    guess = CodeBreaker::Guess.new("1234")
     # Scenarios: 2 numbers correct
     assert_equal("++",    guess.confirm("5254"))
     assert_equal("+-",    guess.confirm("5154"))
     assert_equal("--",    guess.confirm("2545"))
-  end
 
-  def test3_confirm
-    guess = CodeBreaker::Guess.new("1234")
     # Scenarios: 3 numbers correct
     assert_equal("+++",    guess.confirm("5234"))
     assert_equal("++-",    guess.confirm("5134"))
     assert_equal("+--",    guess.confirm("5124"))
     assert_equal("---",    guess.confirm("5123"))
-  end
 
-  def test4_confirm
-    guess = CodeBreaker::Guess.new("1234")
     # Scenarios: all numbers correct
     assert_equal("++++",    guess.confirm("1234"))
     assert_equal("++--",    guess.confirm("1243"))
     assert_equal("+---",    guess.confirm("1423"))
     assert_equal("----",    guess.confirm("4321"))
-  end
 
-  def test_other_confirm
-    guess = CodeBreaker::Guess.new("1234")
     # Scenarios: matches with duplicates
     assert_equal("+",       guess.confirm("1155"))
     assert_equal("-",       guess.confirm("5115"))
     assert_equal("-",       guess.confirm("5115"))
   end
-  def test_other1_confirm
+  def test_1134_confirm
     guess = CodeBreaker::Guess.new("1134")
-    # Scenarios: matches with duplicates
     assert_equal("++",       guess.confirm("1155"))
     assert_equal("+-",       guess.confirm("5115"))
     assert_equal("--",       guess.confirm("5511"))
     assert_equal("++",       guess.confirm("1115"))
     assert_equal("+-",       guess.confirm("5111"))
   end
-  def test_other2_confirm
+  def test_1155_confirm
     guess = CodeBreaker::Guess.new("1155")
-    # Scenarios: matches with duplicates
     assert_equal("+",        guess.confirm("1234"))
   end
-  def test_other3_confirm
+  def test_1111_confirm
     guess = CodeBreaker::Guess.new("1111")
-    # Scenarios: matches with duplicates
     assert_equal("+++",      guess.confirm("1112"))
   end
-  def test_other4_confirm
+  def test_1113_confirm
     guess = CodeBreaker::Guess.new("1113")
-    # Scenarios: matches with duplicates
     assert_equal("++-",      guess.confirm("1121"))
   end
-  def test_other5_confirm
+  def test_3111_confirm
     guess = CodeBreaker::Guess.new("3111")
-    # Scenarios: matches with duplicates
     assert_equal("++--",     guess.confirm("1311"))
   end
-  def test_other6_confirm
+  def test_3114_confirm
     guess = CodeBreaker::Guess.new("3114")
-    # Scenarios: matches with duplicates
     assert_equal("--",       guess.confirm("1251"))
   end
-  def test_other7_confirm
+  def test_1511_confirm
     guess = CodeBreaker::Guess.new("1511")
-    # Scenarios: matches with duplicates
     assert_equal("-",        guess.confirm("2134"))
   end
 end
