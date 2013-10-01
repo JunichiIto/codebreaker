4292495fb890a4cebaa4982b6784b733585b2c6c
Yuji Shimoda <yuji.shimoda@gmail.com>
2013-10-01 15:09:53 +0900

    guess.rb update.

--- guess.rb
+++ guess.rb
@@ -1,49 +1,31 @@
-$result = []
 module CodeBreaker
   class Guess
     def initialize(answer)
-      /(.)(.)(.)(.)/ =~ answer
-      @a1, @a2, @a3, @a4 = $1, $2, $3, $4
+      @ans = []
+      answer.each_char{|c| @ans << c}
     end
     def confirm(guess)
-      /(.)(.)(.)(.)/ =~ guess
-      @g1, @g2, @g3, @g4 = $1, $2, $3, $4
-      $result.clear
-    
-      case @g1
-      when @a1
-        $result << "+"
+      gue = []
+      ret = []
+      guess.each_char{|c| gue << c}
+      ans = @ans.clone
+      @ans.each_with_index do |answer,i|
+        if answer == gue[i]
+          ret << '+'
+          ans[i] = nil
+          gue[i] = nil
+        end
       end
-      case @g2
-      when @a2
-        $result << "+"
+      ans.compact! ; gue.compact!
+      ans.each do |answer|
+        gue.each_with_index do |guess,i|
+          if answer == guess
+            ret << '-'
+            gue.delete_at(i)
+          end
+        end
       end
-      case @g3
-      when @a3
-        $result << "+"
-      end
-      case @g4
-      when @a4
-        $result << "+"
-      end
-      case @g1
-      when @a2, @a3, @a4
-        $result << "-"
-      end
-      case @g2
-      when @a1, @a3, @a4
-        $result << "-"
-      end
-      case @g3
-      when @a1, @a2, @a4
-        $result << "-"
-      end
-      case @g4
-      when @a1, @a2, @a3
-        $result << "-"
-      end
-      return $result.join
+      ret.join
     end
   end
 end
-
