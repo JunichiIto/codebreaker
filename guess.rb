e54e17af099ccb1fc8c7ea96d17b11c962d7bf5b
Yuji Shimoda <yuji.shimoda@gmail.com>
2013-10-01 17:13:42 +0900

    guess.rb refactor.

--- guess.rb
+++ guess.rb
@@ -5,17 +5,17 @@ module CodeBreaker
       answer.each_char{|c| @ans << c}
     end
     def confirm(guess)
-      gue = []
-      ret = []
+      gue = [] ; ret = []
       guess.each_char{|c| gue << c}
       ans = @ans.clone
+      # Exact match number search.
       @ans.each_with_index do |answer,i|
         if answer == gue[i]
           ret << '+'
-          ans[i] = nil
-          gue[i] = nil
+          ans[i] = gue[i] = nil
         end
       end
+      # Exact match number is delete.
       ans.compact! ; gue.compact!
       ans.each do |answer|
         gue.each_with_index do |guess,i|
