b9a3da352754a69778ce65b6cc281c60683229a5
Junichi Ito <jit@sonicgarden.jp>
2013-10-08 05:31:46 +0900

    Organize blank lines

--- guess.rb
+++ guess.rb
@@ -6,6 +6,7 @@ class Guess
     guess_chars = number.split('')
     answer_chars = @answer.split('')
     ret = []
+
     # Exact match number search.
     answer_chars.each_with_index do |ans,i|
       if ans == guess_chars[i]
@@ -13,8 +14,9 @@ class Guess
         answer_chars[i] = guess_chars[i] = nil
       end
     end
-    # Exact match number is delete.
     [answer_chars, guess_chars].map(&:compact!)
+
+    # Exact match number is delete.
     answer_chars.each do |ans|
       guess_chars.each_with_index do |gue,i|
         if ans == gue
@@ -23,6 +25,7 @@ class Guess
         end
       end
     end
+
     ret.join
   end
 end
