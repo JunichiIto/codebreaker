d294a1a34285cf9015a35aec6903cb61b6b38899
Junichi Ito <jit@sonicgarden.jp>
2013-10-08 09:07:04 +0900

    Extract method

--- guess.rb
+++ guess.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class Guess
   def initialize(answer)
     @answer = answer
@@ -6,13 +7,7 @@ class Guess
   def confirm(guess)
     guess_chars, answer_chars = [guess, @answer].map(&:chars).map(&:to_a)
 
-    # Exact match number search.
-    plus_mark = answer_chars.each_with_index.each_with_object('') do |(answer_char, i), mark|
-      if answer_char == guess_chars[i]
-        mark << '+'
-        answer_chars[i] = guess_chars[i] = nil
-      end
-    end
+    plus_mark = search_plus_mark(guess_chars, answer_chars)
 
     # Delete exactly matched numbers
     [answer_chars, guess_chars].map(&:compact!)
@@ -23,6 +18,19 @@ class Guess
           mark << '-'
           guess_chars.delete_at(i)
         end
+      end
+    end
+  end
+
+  private
+
+  # このメソッドを呼ぶと引数の中身が変わるので、あまり良いメソッドではない
+  def search_plus_mark(answer_chars, guess_chars)
+    # Exact match number search.
+    answer_chars.each_with_index.each_with_object('') do |(answer_char, i), mark|
+      if answer_char == guess_chars[i]
+        mark << '+'
+        answer_chars[i] = guess_chars[i] = nil
       end
     end
   end
